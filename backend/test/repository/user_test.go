6a6bf0462c3f1f64e6334225a0379aec4d353769
geek-teru <you@example.com>
2025-01-15 23:29:23 +0900

    modify test code

--- backend/test/repository/user_test.go
+++ backend/test/repository/user_test.go
@@ -187,3 +187,64 @@ func TestGetUserByEmail(t *testing.T) {
 		}
 	}
 }
+
+func TestUpdateUser(t *testing.T) {
+
+	// fixturesの投入
+	loadFixture(t)
+
+	// テストケース
+	tests := []struct {
+		name    string
+		args    *ent.User
+		argsId  int
+		want    *ent.User
+		wanterr error
+	}{
+		{
+			// 正常系
+			name:    "case: Success",
+			args:    testdata.UserTestData[2],
+			argsId:  testdata.UserTestData[2].ID,
+			want:    testdata.UserTestData[2],
+			wanterr: nil,
+		},
+		{
+			// 異常系: 一意制約違反
+			name:    "case: Duplicate error",
+			args:    testdata.UserTestData[3],
+			argsId:  testdata.UserTestData[3].ID,
+			want:    nil,
+			wanterr: fmt.Errorf("[ERROR] failed to update user in repository: ent: constraint failed: pq: duplicate key value violates unique constraint \"users_email_key\""),
+		},
+	}
+
+	repo := repository.NewUserRepository(testClient)
+
+	for _, tt := range tests {
+		fmt.Println(tt.name)
+		//fmt.Println(tt.args) //debug
+		got, goterr := repo.UpdateUser(context.Background(), tt.args, tt.args.ID)
+		//fmt.Println(goterr)
+
+		// 結果の比較
+		if tt.wanterr == nil && goterr == nil {
+			// 正常
+			if diff := cmp.Diff(got, tt.want, cmpopts.IgnoreUnexported(ent.User{})); diff != "" {
+				t.Errorf("[FAIL] return mismatch\n got = %v,\n want= %v\n", got, tt.want)
+			} else {
+				fmt.Println("OK")
+			}
+		} else if tt.wanterr == nil || goterr == nil {
+			// 期待値と結果のどちらか片方がnil
+			t.Errorf("[FAIL] return error mismatch\n goterr = %v,\n wanterr= %v\n", goterr, tt.wanterr)
+		} else {
+			// 異常
+			if goterr.Error() != tt.wanterr.Error() {
+				t.Errorf("[FAIL] return error mismatch\n goterr = %v,\n wanterr= %v\n", goterr, tt.wanterr)
+			} else {
+				fmt.Println("OK")
+			}
+		}
+	}
+}
