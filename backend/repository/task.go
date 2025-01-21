0a966bec7e6473f016f1ca7d20cdd23fb5754c30
geek-teru <you@example.com>
2025-01-22 00:31:51 +0900

    add CreateTask Method to repository

--- backend/repository/task.go
+++ backend/repository/task.go
@@ -2,16 +2,51 @@ package repository
 
 import (
 	"context"
-	//"fmt"
+	"fmt"
 
 	"github.com/geek-teru/simple-task-app/ent"
-	//"github.com/geek-teru/simple-task-app/ent/task"
+	"github.com/geek-teru/simple-task-app/ent/task"
 )
 
 type TaskRepositoryInterface interface {
 	CreateTask(ctx context.Context, task *ent.Task) (*ent.Task, error)
-	ListTask(ctx context.Context, userid int) ([]*ent.Task, error)
-	GetTask(ctx context.Context, taskid int, userid int) (*ent.Task, error)
-	UpdateTask(ctx context.Context, task *ent.Task, taskid int, userid int) (*ent.Task, error)
-	DeleteTask(ctx context.Context, taskid int, userid int) error
+	//ListTask(ctx context.Context, userid int) ([]*ent.Task, error)
+	GetTaskById(ctx context.Context, taskid int, userid int) (*ent.Task, error)
+	//UpdateTask(ctx context.Context, task *ent.Task, taskid int, userid int) (*ent.Task, error)
+	//DeleteTask(ctx context.Context, taskid int, userid int) error
+}
+
+type taskRepository struct {
+	client *ent.Client
+}
+
+func NewTaskRepository(client *ent.Client) TaskRepositoryInterface {
+	return &taskRepository{
+		client: client,
+	}
+}
+
+func (r *taskRepository) CreateTask(ctx context.Context, task *ent.Task) (*ent.Task, error) {
+	createdTask, err := r.client.Task.Create().
+		SetTitle(task.Title).
+		SetDescription(task.Description).
+		SetStatus(task.Status).
+		SetDueDate(*task.DueDate).
+		SetUserID(task.UserID).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("[ERROR] failed to create task in repository: %w", err)
+	}
+	return createdTask, nil
+}
+
+func (r *taskRepository) GetTaskById(ctx context.Context, id int, userId int) (*ent.Task, error) {
+	gotTask, err := r.client.Task.Query().
+		Where(task.IDEQ(id)).
+		Where(task.UserIDEQ(userId)).
+		Only(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("[ERROR] failed to get task by id (%d) in repository: %w", id, err)
+	}
+	return gotTask, nil
 }
