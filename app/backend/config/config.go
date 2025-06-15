5c6607d74cd4ec50003175243a3519b8f394518c
teru <you@example.com>
2025-06-15 22:08:34 +0900

    backend: edit dbconfig

--- app/backend/config/config.go
+++ app/backend/config/config.go
@@ -1,22 +1,32 @@
 package config
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/caarlos0/env/v7"
 )
 
-type Config struct {
-	Env      string `env:"ENV"     envDefault:"dev"`
-	Port     string `env:"PORT"     envDefault:"8080"`
-	LogLevel string `env:"LOG_LEVEL" envDefault:"info"`
-}
+type (
+	Config struct {
+		Env      string `env:"ENV"     envDefault:"dev"`
+		Port     string `env:"PORT"     envDefault:"8080"`
+		LogLevel string `env:"LOG_LEVEL" envDefault:"info"`
+	}
 
-type DBConfig struct {
-	Host     string `env:"DB_HOST"     envDefault:"localhost"`
-	Port     string `env:"DB_PORT"     envDefault:"5432"`
-	DBName   string `env:"DB_NAME"     envDefault:"sampledb"`
-	User     string `env:"DB_USER"     envDefault:"admin"`
-	Password string `env:"DB_PASSWORD" envDefault:"admin"`
-}
+	DBConfig struct {
+		Host     string `env:"DB_HOST"     envDefault:"localhost"`
+		Port     string `env:"DB_PORT"     envDefault:"5432"`
+		DBName   string `env:"DB_NAME"     envDefault:"sampledb"`
+		User     string `env:"DB_USER"     envDefault:"admin"`
+		Password string `env:"DB_PASSWORD" envDefault:"admin"`
+	}
+
+	Secret struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+)
 
 func GetConfig() (Config, error) {
 	config := Config{}
@@ -26,10 +36,24 @@ func GetConfig() (Config, error) {
 	return config, nil
 }
 
-func GetDBConfig() (DBConfig, error) {
-	dbconfig := DBConfig{}
-	if err := env.Parse(&dbconfig); err != nil {
+func GetDBConfig() (*DBConfig, error) {
+	dbconfig := &DBConfig{}
+	err := env.Parse(&dbconfig)
+	if err != nil {
 		panic(err)
 	}
+
+	// AWS Secret Managerで管理されている場合はuser名とパスワードを上書きする
+	dbSecret := os.Getenv("DB_SECRET")
+	if dbSecret != "" {
+		secret := Secret{}
+		err := json.Unmarshal([]byte(dbSecret), &secret)
+		if err != nil {
+			return nil, err
+		}
+		dbconfig.User = secret.Username
+		dbconfig.Password = secret.Password
+	}
+
 	return dbconfig, nil
 }
